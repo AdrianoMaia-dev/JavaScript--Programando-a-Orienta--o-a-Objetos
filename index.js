fa892f8bdeed868909f3c0f11b40b2530e3896dd
AdrianoMaia-dev <dev.adrianomaia@gmail.com>
2021-10-24 19:44:29 -0300

    Continuando os estudos
    Criando comportamentos dentro das classes

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 class Cliente {
     nome;
     cpf;
+}
+class contaCorrente {
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+    depositar(valor){
+        if(valor > 0){
+            this.saldo += valor;
+        }
+    }
 }
+
 const cliente1 = new Cliente()
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+
 
 const cliente2 = new Cliente()
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1)
-console.log(cliente2)
+const contaCorrenteAdriano = new contaCorrente();
+contaCorrenteAdriano.saldo = 0;
+contaCorrenteAdriano.agencia = 1001;
+
+
+contaCorrenteAdriano.depositar(100);
+contaCorrenteAdriano.sacar(50);
+
+console.log(contaCorrenteAdriano)
+
+
